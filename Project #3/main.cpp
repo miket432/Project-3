2b3538901129182a466059182e1e18a0299cd6d2
Michael <miketavella1@gmail.com>
2021-02-11 08:35:50 -0500

    Initial Commit

---
+++ Project #3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project #3
+//
+//  Created by Michael Tavella on 2/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
